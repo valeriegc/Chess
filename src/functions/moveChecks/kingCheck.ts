1062051a1be24ba2f3a2bb6ae4626f5bfa58561d
valeriegc <valerieknape@gmail.com>
2023-07-11 07:30:59 +0200

    king check - refactoring

--- src/functions/moveChecks/kingCheck.ts
+++ src/functions/moveChecks/kingCheck.ts
@@ -14,31 +14,38 @@ import TowerW from '../../pieces/Tower_W.svelte';
 import type { Square } from '../../stores';
 import { castlingCheck } from './castlingCheck';
 
-export const kingCheck = (targetSquare: number, board: Square[], turn: string) => {
-	const columnNumber = columnFinder(targetSquare);
-	let tempArrayLimited: number[];
-	tempArrayLimited = [];
-	const tempArrayInitial = [
-		targetSquare - column,
-		targetSquare + column,
-		targetSquare + row,
-		targetSquare - row,
-		targetSquare - row + 1,
-		targetSquare + row + 1,
-		targetSquare - row - 1,
-		targetSquare + row - 1
-	];
-	if (columnNumber == firstColumn) {
-		tempArrayLimited = tempArrayInitial.filter((n) => columnFinder(n) !== lastColumn);
-	} else if (columnNumber == lastColumn) {
-		tempArrayLimited = tempArrayInitial.filter((n) => columnFinder(n) !== firstColumn);
-	} else {
-		tempArrayLimited = tempArrayInitial;
-	}
+export const kingCheck = (kingLoc: number, board: Square[], turn: string) => {
+	const kingColumn = columnFinder(kingLoc);
+	let kingArray = [];
 	const king = turn == 'black' ? KingB : KingW;
 	const tower = turn == 'black' ? TowerB : TowerW;
-	const castleArr = castlingCheck(king, tower, board);
-	castleArr?.forEach((n) => tempArrayLimited.push(n));
-	const tempArray = tempArrayLimited.filter((n) => n >= 0 && n < biggestSquare);
-	return tempArray.filter((n) => castleArr?.includes(n) || !hasOwnPiece(n, board, turn));
+	const allKingMoves = [
+		kingLoc - column,
+		kingLoc + column,
+		kingLoc + row,
+		kingLoc - row,
+		kingLoc - row + 1,
+		kingLoc + row + 1,
+		kingLoc - row - 1,
+		kingLoc + row - 1
+	];
+	let kingMovesOnBoard = [];
+
+	kingMovesOnBoard = allKingMoves.filter((n) => n >= 0 && n < biggestSquare);
+
+	if (kingColumn == firstColumn) {
+		kingArray = kingMovesOnBoard.filter(
+			(n) => columnFinder(n) !== lastColumn && !hasOwnPiece(n, board, turn)
+		);
+	} else if (kingColumn == lastColumn) {
+		kingArray = kingMovesOnBoard.filter(
+			(n) => columnFinder(n) !== firstColumn && !hasOwnPiece(n, board, turn)
+		);
+	} else {
+		kingArray = kingMovesOnBoard.filter((n) => !hasOwnPiece(n, board, turn));
+	}
+	const castleSquares = castlingCheck(king, tower, board);
+	console.log(kingArray);
+	castleSquares?.forEach((n) => kingArray.push(n));
+	return kingArray;
 };
