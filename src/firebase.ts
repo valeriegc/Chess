c979f978692a72c6fa5cd59aa85fd98a8ccb4462
valeriegc <96831644+valeriegc@users.noreply.github.com>
2023-07-14 12:14:24 +0200

    Firebase config (#17)
    * firebase installed
    
    * set up firebase config

---
+++ src/firebase.ts
@@ -0,0 +1,10 @@
+import { initializeApp } from 'firebase/app';
+const firebaseConfig = {
+	apiKey: 'AIzaSyBvp1CgRFHZU5WQB8MuX_UUBvPqXDln1Vw',
+	authDomain: 'chess-dev-aa3eb.firebaseapp.com',
+	projectId: 'chess-dev-aa3eb',
+	storageBucket: 'chess-dev-aa3eb.appspot.com',
+	messagingSenderId: '587967618814',
+	appId: '1:587967618814:web:299748f262dcb55d1d8faf'
+};
+const app = initializeApp(firebaseConfig);
