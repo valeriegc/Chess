d49bf4972b2d5a3f4b3abd3c0ec2dfef17fd51f5
valeriegc <valerieknape@gmail.com>
2023-08-13 17:16:15 +0200

    fidex bug with check for kingChecked

--- src/functions/kingChecked.ts
+++ src/functions/kingChecked.ts
@@ -4,7 +4,6 @@ import { pawnCheck } from './moveChecks/pawnCheck';
 import { towerCheck } from './moveChecks/towerCheck';
 import { bishopCheck } from './moveChecks/bishopCheck';
 import { kingCheck } from './moveChecks/kingCheck';
-import KingB from '../pieces/King_B.svelte';
 import { row } from '../global';
 
 export const kingChecked = (board: Square[], king: Piece, kingLocation: number) => {
@@ -15,7 +14,8 @@ export const kingChecked = (board: Square[], king: Piece, kingLocation: number) 
 
 	const checkForOpponent = (threatArr: number[], piece: Piece) => {
 		threatArr.forEach((n) => {
-			if (board[n].piece == piece) isKingChecked = true;
+			if (board[n].piece?.type == piece.type && board[n].piece?.color == piece.color)
+				isKingChecked = true;
 		});
 	};
 
@@ -25,36 +25,32 @@ export const kingChecked = (board: Square[], king: Piece, kingLocation: number) 
 	);
 	checkForOpponent(pawnThreatFinal, { type: 'pawn', color: opponentColor });
 
-	const bishopThreat = bishopCheck(kingLocation, board, opponentColor);
+	const bishopThreat = bishopCheck(kingLocation, board, ownColor);
+	console.log(bishopThreat);
 	checkForOpponent(bishopThreat, { type: 'bishop', color: opponentColor });
 
-	const towerThreat = towerCheck(kingLocation, board, opponentColor);
+	const towerThreat = towerCheck(kingLocation, board, ownColor);
 	checkForOpponent(towerThreat, { type: 'tower', color: opponentColor });
 
-	const horseThreat = horseCheck(kingLocation, board, opponentColor);
+	const horseThreat = horseCheck(kingLocation, board, ownColor);
 	checkForOpponent(horseThreat, { type: 'horse', color: opponentColor });
 
 	const queenThreat = bishopThreat.concat(towerThreat);
 	checkForOpponent(queenThreat, { type: 'queen', color: opponentColor });
-
-	if (bishopThreat || towerThreat || horseThreat || queenThreat) isKingChecked = true;
 	console.log(isKingChecked);
 	return isKingChecked;
-};
+}; //3MNGFY2D9QFA5
 
 export const kingCheckMate = (king: Piece, index: number, board: Square[]) => {
 	let remainingKingMoves = [];
-	let turn = '';
-	if (king == KingB) {
-		turn = 'black';
+	let turn = king.color;
+	if ((turn = 'black')) {
 		remainingKingMoves = kingCheck(index, board, turn);
 		const allowedKingMoves = remainingKingMoves.filter((n) => !kingChecked(board, king, n));
 		if (allowedKingMoves.length == 0) return true;
 	} else {
-		turn = 'white';
 		remainingKingMoves = kingCheck(index, board, turn);
 		const allowedKingMoves = remainingKingMoves.filter((n) => !kingChecked(board, king, n));
-
 		if (allowedKingMoves.length == 0) return true;
 	}
 	return false;
