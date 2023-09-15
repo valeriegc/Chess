a1d3563d22d24b80ea5c96505a5123c6854a3c8d
valeriegc <valerieknape@gmail.com>
2023-09-15 13:32:23 +0200

    css improvements on game details + added links for chess info

--- src/components/GameDetails.svelte
+++ src/components/GameDetails.svelte
@@ -15,24 +15,34 @@
 	};
 </script>
 
-<div class="selectWrap">
-	<button on:click={() => goto('/game')}>Start a game</button>
+<div class="wrap">
 	<div>
-		<p style="font-size:1.5rem;">Statistics</p>
+		<p style="font-size:1.5rem;">Game statistics</p>
 		<div style="margin-left:1.5rem">
 			<p>Games Played: {played}</p>
 			<p>Lost: {lost}</p>
 			<p>Won: {won}</p>
 		</div>
-		<p style="font-size:1.5rem;">Friends</p>
 	</div>
-	<button on:click={signOutSSR}>Sign out</button>
+	<div class="btnWrap">
+		<button on:click={signOutSSR}>Sign out</button>
+		<button on:click={() => goto('/game')}>Start a game</button>
+	</div>
+	<div>
+		<p style="font-size:1.5rem">Information on Chess</p>
+		<ol>
+			<li><a href="https://en.wikipedia.org/wiki/Rules_of_chess"> Rules of Chess explained</a></li>
+			<li>
+				<a href="https://www.youtube.com/watch?v=OCSbzArwB10">Comprehensive video for beginners</a>
+			</li>
+		</ol>
+	</div>
 </div>
 
 <style>
-	.selectWrap {
+	.wrap {
 		background-color: rgba(0, 0, 0, 0.562);
-		padding: 2rem;
+		padding: 1.5rem;
 		grid-column-start: 2;
 		grid-column-end: 3;
 		grid-row-start: 1;
@@ -41,6 +51,7 @@
 	}
 	button {
 		background-color: rgb(75, 79, 87);
+		padding-inline: 1rem;
 	}
 	button:hover {
 		background-color: var(--secondary);
@@ -49,5 +60,15 @@
 	}
 	p {
 		color: whitesmoke;
+	}
+	.btnWrap {
+		margin: auto;
+		margin-block: 3rem;
+		display: flex;
+		justify-content: center;
+		gap: 1rem;
+	}
+	li {
+		margin-bottom: 1rem;
 	}
 </style>
